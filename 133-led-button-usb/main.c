76d32c815cb8b808590067149e71fef202c00ebc
Viktor Rusanov <rusanov079@gmail.com>
2026-09-17 00:50:04 +0300

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -17,6 +17,22 @@ void set_led(bool on) {
     printf("led %s\n", on ? "on" : "off");
 }
 
+bool handle_command(int command, bool led) {
+    if (command == 'e') {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd') {
+        led = false;
+        set_led(led);
+    }
+    else {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
+}
+
 int main()
 {
     stdio_init_all();
@@ -38,5 +54,13 @@ int main()
             set_led(led);
         }
         previous = current;
+        
+        int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT) {
+            continue;
+        }
+
+        led = handle_command(command, led);
    }
 }
